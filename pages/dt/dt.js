d1195ef28d35ef2e22afef021554f7d8f61404b9
XiJuanBaHuang <3380086416@qq.com>
2022-11-21 20:45:08 +0800

    动态页面的用户登录基本实现
    实现了只允许用户一次登录（即用户登录后，不应再次登录）；
    准备将 lcy-timeline 数据库中已有的几份数据导入现有的动态页面。

--- pages/dt/dt.js
+++ pages/dt/dt.js
@@ -5,40 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-    key1: true,//是否未授权登录
-    userInfo:'',//用户信息
-    text1:"这猫真可爱",//配文，使用时删除样例文字
-    photo1:"/pages/08.png",//配图，使用时删除样例路径
-    userid1:"世界先生",//其他用户ID，推荐换成数组存储，使用时删除样例ID
-    path:"/pages/08.png"//其他用户头像，推荐换成数组存储，使用时删除样例头像
+    key1: true, //是否未授权登录
+    userInfo: '', //用户信息
+    text1: "这猫真可爱", //配文，使用时删除样例文字
+    photo1: "/pages/08.png", //配图，使用时删除样例路径
+    userid1: "世界先生", //其他用户ID，推荐换成数组存储，使用时删除样例ID
+    path: "/pages/08.png" //其他用户头像，推荐换成数组存储，使用时删除样例头像
   },
 
-  jumpPage(){
+  jumpPage() {
     wx.navigateTo({
       url: '/pages/jie1/jie1',
     })
   },
   //获取用户信息
-  getProfile(){
+  getProfile() {
     wx.getUserProfile({
-      desc:'必须授权后才能继续使用',
-      success:res=>{
-        let user=res.userInfo
+      desc: '必须授权后才能继续使用',
+      success: res => {
+        let user = res.userInfo
         //设置本地缓存,把用户信息缓存到本地
-        wx.setStorageSync('user',user)
+        wx.setStorageSync('user', user)
         this.setData({
-            userInfo:user,
-            key1:false
+          userInfo: user,
+          key1: false
         })
       }
     })
   },
   //展开评论
-  comment(){
+  comment() {
 
   },
   //点赞
-  like(){
+  like() {
 
   },
 })
---
+++ pages/temp/temp.js
@@ -0,0 +1,68 @@
+// pages/temp/temp.js
+
+let app = getApp();
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        hasUserInfo: false, //是否未授权登录
+        userInfo: '', //用户信息
+        // text1: "这猫真可爱", //配文，使用时删除样例文字
+        // photo1: "/pages/08.png", //配图，使用时删除样例路径
+        // userid1: "世界先生", //其他用户ID，推荐换成数组存储，使用时删除样例ID
+        // path: "/pages/08.png", //其他用户头像，推荐换成数组存储，使用时删除样例头像
+        dataList: []
+    },
+
+    onLoad() {
+        let that = this;
+        wx.cloud.database().collection('lcy-timeline')
+            .orderBy('createTime', 'desc') //按发布视频排序
+            .get({
+                success(res) {
+                    console.log("请求成功", res)
+                    that.setData({
+                        dataList: res.data
+                    })
+                },
+                fail(res) {
+                    console.log("请求失败", res)
+                }
+            })
+    },
+
+    jumpPage() {
+        wx.navigateTo({
+            url: '/pages/jie1/jie1',
+        })
+    },
+    //获取用户信息
+    getProfile() {
+        // 不允许多次登录
+        if (this.data.hasUserInfo === true)
+            return
+        wx.getUserProfile({
+            desc: '必须授权后才能继续使用',
+            success: res => {
+                let user = res.userInfo
+                //设置本地缓存,把用户信息缓存到本地
+                wx.setStorageSync('user', user)
+                this.setData({
+                    userInfo: user,
+                    hasUserInfo: true
+                })
+            }
+        })
+    },
+    //展开评论
+    comment() {
+
+    },
+    //点赞
+    like() {
+
+    },
+})
